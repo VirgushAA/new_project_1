1b237e3b8cb7bfe2c5163c626cb37d625133db48
Vindicator Onita <vindicao@in-i2.nsk.21-school.ru>
2026-02-09 18:08:04 +0700

    start html API

--- data/loader.py
+++ data/loader.py
@@ -1,3 +1,26 @@
+import argparse
+import requests
+
+def LoadData(args: argparse.Namespace):
+    if args.__dict__['method'] == 'html':
+        data = LoadFromSite(args)
+    else:
+        data = None
+
+    return data
+
+def LoadFromSite(args: argparse.Namespace):
+    urls = {
+        'cur': 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}',
+        'history': ''
+    }
+    req = requests.get('http://...')
+    return req
+
+
+
+
+
 # Цель:
 
 # Научиться:
--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('city', help='location')
+parser.add_argument('days', help='time period')
+parser.add_argument('-m', '--method', default='html', help='data accuireing? method')
+parser.add_argument('metric', nargs='?', default='aboba', help='metrics idk')
+args = parser.parse_args()
 
 def main():
-    data = fetch_weather("Amsterdam")
-    df = to_dataframe(data)
 
-    stats = analyze(df)
+    if args.__dict__['method'] == 'html':
+        pass
+    # data = fetch_weather("Amsterdam")
+    # df = to_dataframe(data)
+    #
+    # stats = analyze(df)
+    #
+    # print(stats)
+    # plot(df)
+    print(type(args))
 
-    print(stats)
-    plot(df)
 
---city
---days
---metric
+if __name__ == '__main__':
+    main()
