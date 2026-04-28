3d73d2217b97a6662eff84b072b548d939eff155
VirgushAA <vaa2024@ya.ru>
2026-04-28 19:00:21 +0700

    paused

--- data/loader.py
+++ data/loader.py
@@ -1,40 +1,48 @@
 import argparse
 import requests
+import pandas as pd
+import json
 
-def LoadData(args: argparse.Namespace):
-    if args.__dict__['method'] == 'html':
-        data = LoadFromSite(args)
-    else:
-        data = None
 
-    return data
+API_KEY = "c4beee2a4cb9972e23ddb33e3d9aaf2a" # спрятать потом в env
 
-def LoadFromSite(args: argparse.Namespace):
+lat = 33.44 # (-90; 90)
+lon = -94.04 # (-180; 180)
+
+def load_data(args: argparse.Namespace):
+    if args.method == 'api':
+        raw = load_from_site(args)
+        return to_dataframe(raw)
+
+    if args.method == 'file':
+        pass
+
+    raise ValueError('Method not implemented')
+
+
+def load_from_site(args: argparse.Namespace):
     urls = {
-        'cur': 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}',
+        'cur': "https://api.open-meteo.com/v1/forecast?"
+                "latitude=52.52&longitude=13.41"
+                "&daily=temperature_2m_max,temperature_2m_min"
+                "&hourly=temperature_2m,wind_speed_10m"
+                "&timezone=auto",
+        'city': f"https://geocoding-api.open-meteo.com/v1/search?name={args.city}",
         'history': ''
     }
-    req = requests.get('http://...')
+    if args.city == 'default':
+        req = requests.get(urls['cur']).json()
+    else:
+        req = requests.get(urls['city']).json()
+    # print('-----------------------') debug response
+    print(json.dumps(req, indent=4))
+    # print('-----------------------')
     return req
 
+def to_dataframe(data):
+    # return pd.DataFrame(data)
 
-
-
-
-# Цель:
-
-# Научиться:
-#
-# получать данные
-#
-# класть в DataFrame
-#
-# считать
-#
-# Функции:
-#
-# fetch_weather()
-#
-# analyze()
-#
-# plot()
+    try:
+        return pd.DataFrame(data['hourly'])
+    finally:
+        return pd.DataFrame(data)
--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import argparse
-from data.loader import LoadData
+from data.loader import load_data
 
 parser = argparse.ArgumentParser()
-parser.add_argument('city', help='location')
+parser.add_argument('city', default='default', help='location')
 parser.add_argument('days', help='time period')
-parser.add_argument('-m', '--method', default='html', help='data accuireing? method')
+parser.add_argument('-m', '--method', default='api', help='data accuireing? method')
 parser.add_argument('metric', nargs='?', default='aboba', help='metrics idk')
 args = parser.parse_args()
 
 def main():
 
-    data = LoadData(args)
-    # data = fetch_weather("Amsterdam")
-    # df = to_dataframe(data)
-    #
-    # stats = analyze(df)
-    #
-    # print(stats)
-    # plot(df)
-    print(type(args))
+    df_weather = load_data(args)
+
+
+    # print(type(args))
+    print(args)
+    print(df_weather)
+    print(df_weather.describe())
 
 
 if __name__ == '__main__':
